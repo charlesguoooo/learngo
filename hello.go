db41c154483046fa63b4bb0d47c0afc64ef51c28
az_a_tguocl <az_a_tguocl@cn133.azure.net>
2019-04-18 09:38:21 +0800

    add new func

--- hello.go
+++ hello.go
@@ -8,4 +8,11 @@ import (
 
 func main() {
 	fmt.Print(stringutil.Reverse("\n!oG,olleH"))
+	stringutil.Package()
+	stringutil.Import()
+	fmt.Println(stringutil.Add(43, 12))
+	a, b := stringutil.Swap("hello", "myboy")
+	fmt.Println(a, b)
+	ns1, ns2 := stringutil.Split(17)
+	println(ns1, ns2)
 }
