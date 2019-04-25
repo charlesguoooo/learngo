9a3bfd0366d6d5e1cd2e5a3df800f02f2be84f76
az_a_tguocl <az_a_tguocl@cn133.azure.net>
2019-04-25 18:21:05 +0800

    update learn date 4.25

--- hello.go
+++ hello.go
@@ -1,18 +1,31 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/charlesguoooo/stringutil"
-)
+import "github.com/charlesguoooo/stringutil"
 
 func main() {
-	fmt.Print(stringutil.Reverse("\n!oG,olleH"))
-	stringutil.Package()
-	stringutil.Import()
-	fmt.Println(stringutil.Add(43, 12))
-	a, b := stringutil.Swap("hello", "myboy")
-	fmt.Println(a, b)
-	ns1, ns2 := stringutil.Split(17)
-	println(ns1, ns2)
+	//fmt.Print(stringutil.Reverse("\n!oG,olleH"))
+	//stringutil.Package()
+	//stringutil.Import()
+	//fmt.Println(stringutil.Add(43, 12))
+	//a, b := stringutil.Swap("hello", "myboy")
+	//fmt.Println(a, b)
+	//ns1, ns2 := stringutil.Split(20)
+	//println(ns1, ns2)
+	//stringutil.Vab()
+	//stringutil.Vab2()
+	//stringutil.Vab3()
+	//stringutil.Bt()
+	//stringutil.Zero()
+	//stringutil.Tc()
+	//stringutil.Ti()
+	//stringutil.Nc()
+	//stringutil.FF()
+	//stringutil.Is()
+	//stringutil.IAS()
+	//stringutil.ST()
+	//stringutil.SC()
+	//stringutil.SS2()
+	//stringutil.PT()
+	//stringutil.V1()
+	stringutil.MS()
 }
