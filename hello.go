52e5977d85763e49011dc2d503bc19fec66c33b6
az_a_tguocl <az_a_tguocl@cn133.azure.net>
2019-05-24 14:57:08 +0800

    update

--- hello.go
+++ hello.go
@@ -3,29 +3,7 @@ package main
 import "github.com/charlesguoooo/stringutil"
 
 func main() {
-	//fmt.Print(stringutil.Reverse("\n!oG,olleH"))
-	//stringutil.Package()
-	//stringutil.Import()
-	//fmt.Println(stringutil.Add(43, 12))
-	//a, b := stringutil.Swap("hello", "myboy")
-	//fmt.Println(a, b)
-	//ns1, ns2 := stringutil.Split(20)
-	//println(ns1, ns2)
-	//stringutil.Vab()
-	//stringutil.Vab2()
-	//stringutil.Vab3()
-	//stringutil.Bt()
-	//stringutil.Zero()
-	//stringutil.Tc()
-	//stringutil.Ti()
-	//stringutil.Nc()
-	//stringutil.FF()
-	//stringutil.Is()
-	//stringutil.IAS()
-	//stringutil.ST()
-	//stringutil.SC()
-	//stringutil.SS2()
-	//stringutil.PT()
-	//stringutil.V1()
-	stringutil.MS()
+	stringutil.GRT()
 }
+
+
